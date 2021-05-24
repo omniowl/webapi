eb685427536afcf7afbd6d43a458259218b89dbb
OmniOwl <xenon373@gmail.com>
2021-05-24 14:05:29 +0200

    Changed name of Field from OldPassword to CurrentPassword
    Makes it more clear what the password actually is. Because while it will be the old password after the password change, it is the current password before then,

--- BookApp/BookApp/Models/AccountBindingModels.cs
+++ BookApp/BookApp/Models/AccountBindingModels.cs
@@ -18,7 +18,7 @@ namespace BookApp.Models
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Current password")]
-        public string OldPassword { get; set; }
+        public string CurrentPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
